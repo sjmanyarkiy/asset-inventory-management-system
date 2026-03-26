95e172e42543957008351c420f7196eb4a5e8955
ajok <ajokd6330@gmail.com>
2026-03-26 23:42:37 +0300

    Ready for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
-    return {"message": "Backend is running!"}
+    return jsonify({"message": "Backend is running!"})
+
+if __name__ == "__main__":
+    app.run(debug=False)
