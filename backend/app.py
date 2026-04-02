b41de4a4a0180750abf89cc19367231ef8ce4956
commit-panda <sjmanyarkiy@gmail.com>
2026-04-02 19:14:16 +0300

    added registration feature branch

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from blueprints.auth import auth_bp
 import os
+
 
 app = Flask(__name__)
 CORS(app)
+
+app.register_blueprint(auth_bp)
 
 @app.route("/")
 def home():
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
-  return <h1>Asset Inventory System</h1>
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './api/axiosConfig';
+
+import RegisterPage from './pages/RegistrationPage';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* public route */}
+          <Route path="registration" element={<RegistrationPage />}/>
+
+          {/* protected route */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <DashboardPage/>
+            </ProtectedRoute>
+          }
+          />
+
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Router>
+    </Provider>
+  )
 }
 
 export default App
