af28b19689eadc3a4d15fa4d468eb6f3ba8de2f3
commit-panda <sjmanyarkiy@gmail.com>
2026-04-20 16:21:30 +0300

    modified api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,14 @@
 // import axios from "axios";
 import axios from "../../src/api/axios";
 
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5000",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
