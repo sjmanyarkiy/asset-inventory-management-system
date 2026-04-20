047967b8b439fc2ccf0e2c20ff9361acbb91c24f
commit-panda <sjmanyarkiy@gmail.com>
2026-04-20 10:08:45 +0300

    changed token to access_token in axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,7 +7,7 @@ const instance = axios.create({
 
 // attach token if present
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("access_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
